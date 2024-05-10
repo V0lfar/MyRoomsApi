462dc2cffa57b2f684728494d68d8927d3f5b304
V0lfar <vlad.volfar@gmail.com>
2024-05-10 19:27:28 +0300

    Init

---
+++ src/main/java/my/rooms/vif/MyRoomsApiApplication.java
@@ -0,0 +1,13 @@
+package my.rooms.vif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRoomsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRoomsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/rooms/vif/MyRoomsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package my.rooms.vif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRoomsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
